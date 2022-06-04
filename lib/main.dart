31eae40a50a0c6680e28c2b2412945c1b178f2dc
benjamin <benjaminportilo1998@gmail.com>
2022-06-04 13:16:19 -0600

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:delivery_app/bindings/home_binding.dart';
+import 'package:delivery_app/login/Screens/Login_Screen.dart';
 import 'package:delivery_app/screens/splash.screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,6 +27,6 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Delivery App',
             theme: ThemeData(primarySwatch: Colors.blue),
-            home: const SplashScreen()));
+            home: LoginScreen()));
   }
 }
